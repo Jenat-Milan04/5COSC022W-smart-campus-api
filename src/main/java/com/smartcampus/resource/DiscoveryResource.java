0d0db8f45b75c6d8d3578e99f483aa92802b7460
Jenat-Milan04 <jenatmilan05@gmail.com>
2026-04-21 09:44:31 +0530

    discovery resource code

--- src/main/java/com/smartcampus/resource/DiscoveryResource.java
+++ src/main/java/com/smartcampus/resource/DiscoveryResource.java
@@ -1,5 +1,36 @@
 package com.smartcampus.resource;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
+
+@Path("/")
 public class DiscoveryResource {
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response discover() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("api", "Smart Campus API");
+        response.put("version", "1.0");
+        response.put("contact", "admin@smartcampus.ac.uk");
+
+        Map<String, String> resources = new HashMap<>();
+        resources.put("rooms", "/api/v1/rooms");
+        resources.put("sensors", "/api/v1/sensors");
+        response.put("resources", resources);
+
+        Map<String, String> links = new HashMap<>();
+        links.put("self", "/api/v1");
+        links.put("rooms", "/api/v1/rooms");
+        links.put("sensors", "/api/v1/sensors");
+        response.put("_links", links);
+
+        return Response.ok(response).build();
+    }
 }
+
